4473b9e90a46573f80404aa7901e4bc1ee824eaf
Alkhatep <abdulla.sayed94@gmail.com>
2024-01-08 01:47:08 +0200

    make a route and link to it

--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-
-<title>My Blog</title>
-<link rel="stylesheet" type="text/css" href="/app.css">
-<script type="text/javascript" src="/app.js"></script>
-
-<body>
-    <h1>Hello World</h1>
-</body>
