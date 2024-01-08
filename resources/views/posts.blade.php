c32442a18437bfafef518e6e90e1ac6169840465
Alkhatep <abdulla.sayed94@gmail.com>
2024-01-08 03:12:07 +0200

    use a file system class to read a directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,40 +4,9 @@
 <link rel="stylesheet" type="text/css" href="/app.css">
 
 <body>
-
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper tincidunt augue in sodales. Sed consequat eu orci sit amet vulputate. Duis consequat egestas dui, ac dictum metus. Pellentesque efficitur nunc mauris, eu pulvinar purus scelerisque quis. Sed at tempor turpis. Curabitur id purus semper, cursus ex a, ultrices nisl. Nam efficitur maximus nisi, eget vestibulum ipsum ullamcorper ac.
-
-            Ut mattis arcu elementum lectus ultrices, eu facilisis sapien congue. Aliquam egestas lobortis felis, eu porta neque gravida sed. Nullam volutpat tristique risus ac fermentum. Nunc sollicitudin, nisl porta egestas tincidunt, lectus tortor commodo velit, et gravida risus turpis eu justo. Nulla id pharetra lectus, vel maximus lectus. Nulla facilisi. Pellentesque cursus diam lacus, ac hendrerit nunc scelerisque sit amet. Maecenas gravida consectetur odio sit amet hendrerit. Cras in semper felis. Nullam in tellus enim. In hac habitasse platea dictumst.
-
-
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper tincidunt augue in sodales. Sed consequat eu orci sit amet vulputate. Duis consequat egestas dui, ac dictum metus. Pellentesque efficitur nunc mauris, eu pulvinar purus scelerisque quis. Sed at tempor turpis. Curabitur id purus semper, cursus ex a, ultrices nisl. Nam efficitur maximus nisi, eget vestibulum ipsum ullamcorper ac.
-
-            Ut mattis arcu elementum lectus ultrices, eu facilisis sapien congue. Aliquam egestas lobortis felis, eu porta neque gravida sed. Nullam volutpat tristique risus ac fermentum. Nunc sollicitudin, nisl porta egestas tincidunt, lectus tortor commodo velit, et gravida risus turpis eu justo. Nulla id pharetra lectus, vel maximus lectus. Nulla facilisi. Pellentesque cursus diam lacus, ac hendrerit nunc scelerisque sit amet. Maecenas gravida consectetur odio sit amet hendrerit. Cras in semper felis. Nullam in tellus enim. In hac habitasse platea dictumst.
-
-
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper tincidunt augue in sodales. Sed consequat eu orci sit amet vulputate. Duis consequat egestas dui, ac dictum metus. Pellentesque efficitur nunc mauris, eu pulvinar purus scelerisque quis. Sed at tempor turpis. Curabitur id purus semper, cursus ex a, ultrices nisl. Nam efficitur maximus nisi, eget vestibulum ipsum ullamcorper ac.
-
-            Ut mattis arcu elementum lectus ultrices, eu facilisis sapien congue. Aliquam egestas lobortis felis, eu porta neque gravida sed. Nullam volutpat tristique risus ac fermentum. Nunc sollicitudin, nisl porta egestas tincidunt, lectus tortor commodo velit, et gravida risus turpis eu justo. Nulla id pharetra lectus, vel maximus lectus. Nulla facilisi. Pellentesque cursus diam lacus, ac hendrerit nunc scelerisque sit amet. Maecenas gravida consectetur odio sit amet hendrerit. Cras in semper felis. Nullam in tellus enim. In hac habitasse platea dictumst.
-
-
-        </p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
