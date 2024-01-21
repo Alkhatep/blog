765b9d5aa530199776cb58bf22f3b502be91a508
Alkhatep <abdulla.sayed94@gmail.com>
2024-01-21 13:06:47 +0200

    bulid the log in Page

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,7 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionController extends Controller {
+	public function create() {
+		return view('sessions.create');
+	}
+
+	public function store() {
+		$attributes = request()->validate([
+			'email' => 'required',
+			'password' => 'required',
+		]);
+
+		if (auth()->attempt($attributes)) {
+			return redirect('/')->with('success', 'Welcom Back!');
+		}
+
+		throw ValidationException::withMessages([
+			'email' => 'your provided credentials could not be verified',
+		]);
+	}
+
 	public function destroy() {
 		auth()->logout();
 
