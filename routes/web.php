165b126116c995b01a4747f6df107ce55863d03a
Alkhatep <abdulla.sayed94@gmail.com>
2024-01-28 16:06:58 +0200

    Make the newsletter signup form work

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('ping', function () {
+Route::post('newsletter', function () {
+	request()->validate(['email' => 'required|email']);
+
 	$mailchimp = new \MailchimpMarketing\ApiClient();
 
 	$mailchimp->setConfig([
@@ -14,11 +16,17 @@ Route::get('ping', function () {
 		'server' => 'us17',
 	]);
 
-	$response = $mailchimp->lists->addListMember('65cb51d4de', [
-		'email_address' => '3bdulladulla@gmail.com',
-		'status' => 'subscribed',
-	]);
-	ddd($response);
+	try {
+		$response = $mailchimp->lists->addListMember('65cb51d4de', [
+			'email_address' => request('email'),
+			'status' => 'subscribed',
+		]);
+	} catch (\Exception $e) {
+		throw \Illuminate\Validation\ValidationException::withMessages([
+			'email' => 'This email could not be added to our newsletter list.',
+		]);
+	}
+	return redirect('/')->with('success', 'You are now signed up for our newsletter!');
 });
 
 Route::get('/', [PostController::class, 'index'])->name('home');
